3c9d38b93e075363c17bff4d1806b773750ce45d
Gonzalo Rivero <griverorz@gmail.com>
2019-11-30 10:29:01 -0500

    Update to new definition

--- R/cisutils.R
+++ R/cisutils.R
@@ -1,13 +1,13 @@
 #' Read data and JSON
+#'
 #' @export
 #' @import haven jsonlite
 input <- function(json, data) {
     rules <- fromJSON(readLines(json))
     
     ## Data
-    data <- read_spss(data)
+    data <- as.data.frame(read_spss(data))
     
-    valid_instructions(rules, names(data))
     class(data) <- append(class(data), "cis")
     attributes(data)$json <- rules
     attributes(data)$renamed <- FALSE
@@ -15,60 +15,30 @@ input <- function(json, data) {
 }
 
 
-#' Test JSON
-#' @export
-valid_instructions <- function(x, ndata) {
-    
-    if (length(setdiff(names(x$variables), ndata)) != 0) {
-        stop("Variables in the JSON file do not exist in the data")
-    }
-    
-    if(all(names(x$variables) != names(x$missing))) {
-        stop("Mismatch between defined and missing variables")
-    }
-    
-    return(NULL)    
-}
-
-
 #' Recode according to instructions
 #' @export
-recode_missings <- function(data) {
-    if (!"cis" %in% class(data)) {
-        stop("Only allows CIS data")
-    }
-    
-    x <- attr(data, "json")
-    
-    for (i in 1:length(x$variables)) {
-        misvals <- unlist(x$missing[i])
-        varname <- names(x$missing)[i]
-        
+recode_missing <- function(data) {
+    json <- attributes(data)$json
+    for (i in seq_along(json$variables)) {
+        var <- names(json$variables)[i]
+        misvals <- unlist(json$variables[[i]]$missing)
         if (!is.null(misvals)) {
-            data[[varname]][data[[varname]] %in% misvals] <- NA
+            data[[var]][data[[var]] %in% misvals] <- NA
         }
     }
-    
     return(data)
 }
-
 
 #' Rename variables
 #' @export
 rename_variables <- function(data) {
-    if (!"cis" %in% class(data)) {
-        stop("Only allows CIS data")
-    }
-    
-    x <- attr(data, "json")
-
-    for (i in 1:length(x$variables)) {
-        oldname <- names(x$variables)[i]
-        newname <- unlist(x$variables[i])
+    json <- attributes(data)$json
+    for (i in seq_along(json$variables)) {
+        oldname <- names(json$variables)[i]
+        newname <- json$variables[[i]]$label
         data[[newname]] <- data[[oldname]]
     }
     
-    attributes(data)$renamed <- TRUE
     return(data)
 }
 
@@ -77,20 +47,15 @@ rename_variables <- function(data) {
 #' @export
 #' @import dplyr
 recode_values <- function(data) {
-    if (!"cis" %in% class(data)) {
-        stop("Only allows CIS data")
-    }
-
-    if (!attr(data, "renamed")) {
-        stop("Must rename variables first")
-    }
-    
-    x <- attr(data, "json")
-
-    for (i in 1:length(x$recodes)) {
-        varname <- names(x$recodes)[i]
-        data[[varname]] <- as.integer(data[[varname]])
-        data[[varname]] <- recode(data[[varname]], !!!x$recodes[[varname]], .default="other")
+    json <- attributes(data)$json
+    for (i in seq_along(json$variables)) {
+        var <- names(json$variables)[i]
+        data[[var]] <- as.integer(data[[var]])
+        if (!is.null(json$variables[[i]]$recodes)) {
+            data[[var]] <- recode(data[[var]],
+                                  !!!json$variables[[i]]$recodes,
+                                  .default="other")
+        }
     }
     return(data)
 }
@@ -100,22 +65,13 @@ recode_values <- function(data) {
 #' @export
 #' @import dplyr
 known_recodes <- function(data) {
-    if (!"cis" %in% class(data)) {
-        stop("Only allows CIS data")
-    }
-    
-    if (!attr(data, "renamed")) {
-        stop("Must rename variables first")
-    }
-    
-    x <- attr(data, "json")
 
     if (!all(c("gender", "age", "ccaa") %in% names(data))) {
         stop("Variables gender, age, and ccaa must exist in renamed data")
     }
     
     ## Gender
-    data$gender <- recode(as.numeric(data$gender), "1"="man", "2"="woman")
+    data$gender <- recode(as.character(data$gender), "1"="man", "2"="woman")
 
     ## Age
     data$age[data$age %in% c(0:24)] <- "age24"
@@ -128,24 +84,24 @@ known_recodes <- function(data) {
     ## CCAA
     data$ccaa <- as_factor(data$ccaa)
     levels(data$ccaa) <- c("andalucia",
-                          "aragon",
-                          "asturias",
-                          "balears",
-                          "canarias",
-                          "cantabria",
-                          "castilla-lamancha",
-                          "castilla-leon",
-                          "cataluna",
-                          "valencia",
-                          "extremadura",
-                          "galicia",
-                          "madrid",
-                          "murcia",
-                          "navarra",
-                          "paisvasco",
-                          "rioja",
-                          "ceuta",
-                          "melilla")
+                           "aragon",
+                           "asturias",
+                           "balears",
+                           "canarias",
+                           "cantabria",
+                           "castilla-lamancha",
+                           "castilla-leon",
+                           "cataluna",
+                           "valencia",
+                           "extremadura",
+                           "galicia",
+                           "madrid",
+                           "murcia",
+                           "navarra",
+                           "paisvasco",
+                           "rioja",
+                           "ceuta",
+                           "melilla")
     return(data)
 }
 
@@ -153,31 +109,22 @@ known_recodes <- function(data) {
 #' Ensure appropriate types
 #' @export
 retype <- function(data) {
-    if (!"cis" %in% class(data)) {
-        stop("Only allows CIS data")
-    }
-    
-    if (!attr(data, "renamed")) {
-        stop("Must rename variables first")
-    }
-    
-    x <- attr(data, "json")
-
-    for (i in 1:length(x$types)) {
-        varname <- names(x$types)[i]
-        newtype <- unlist(x$types[i])
-        if (newtype == "numeric") {
-            data[[varname]] <- as.numeric(data[[varname]])
-        }
-        if (newtype == "ordered") {
-            data[[varname]] <- factor(data[[varname]], ordered=TRUE)
-        }
-    }
-
-    for (i in 1:ncol(data)) {
-        if ("character" %in% class(data[, i])) {
-            data[, i] <- factor(data[, i])
-        }
+    json <- attributes(data)$json
+    for (i in seq_along(json$variables)) {
+        var <- names(json$variables)[i]
+        newtype <- json$variables[[i]]$type
+        if (is.null(newtype)) {
+            if ("character" %in% class(data[var])) {
+                data[[var]] <- factor(data[[var]])
+            }            
+        } else {
+            if (newtype == "numeric") {
+                data[[var]] <- as.numeric(data[[var]])
+            }
+            if (newtype == "ordered") {
+                data[[var]] <- factor(data[[var]], ordered=TRUE)
+            }
+        }                
     }
 
     return(data)
@@ -187,16 +134,8 @@ retype <- function(data) {
 #' Keep only relevant variables
 #' @export
 subselect <- function(data) {
-    if (!"cis" %in% class(data)) {
-        stop("Only allows CIS data")
-    }
-    
-    if (!attr(data, "renamed")) {
-        stop("Must rename variables first")
-    }
-    
-    x <- attr(data, "json")
-
-    data <- data[, unlist(x$variables)]
+    json <- attributes(data)$json
+    nn <- unlist(lapply(json$variables, function(x) x$label))
+    data <- data[, nn]
     return(data)
 }
